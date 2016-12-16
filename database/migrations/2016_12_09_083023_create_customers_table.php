58e322cc22fec58848d0e2b26e5085cfe009bf0b
thanhnt <tatthanh.dev@gmail.com>
2016-12-16 17:24:22 +0700

    #1: update customer & employee model

--- database/migrations/2016_12_09_083023_create_customers_table.php
+++ database/migrations/2016_12_09_083023_create_customers_table.php
@@ -18,6 +18,8 @@ class CreateCustomersTable extends Migration
             $table->string('name');
             $table->string('address');
             $table->string('telephone');
+            $table->integer('province_id')->default(0);
+            $table->integer('district_id')->default(0);
             $table->bigInteger('avatar_image_id')->default(0);
 
             $table->softDeletes();
