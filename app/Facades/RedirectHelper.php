80808cd7f1a3b9cc99f16e34f0723fe0f923553f
thanhnt <tatthanh.dev@gmail.com>
2017-01-06 18:26:56 +0700

    #30: Support Redirect Guest For multiauth

---
+++ app/Facades/RedirectHelper.php
@@ -0,0 +1,11 @@
+<?php namespace App\Facades;
+
+use Illuminate\Support\Facades\Facade;
+
+class RedirectHelper extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'App\Helpers\RedirectHelperInterface';
+    }
+}
